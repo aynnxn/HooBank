56694491cf2e741b80f7b281daa1cd4f11fe7a1b
aynnxn <aymanbolad@pm.me>
2022-08-18 00:03:01 -0400

    fixed hero, nav, get started components

--- NuuBank/src/components/GetStarted.jsx
+++ NuuBank/src/components/GetStarted.jsx
@@ -1,4 +1,4 @@
-import styles from "../styles";
+import styles from "../style";
 import { arrowUp } from "../assets";
 
 const GetStarted = () => (
@@ -9,17 +9,17 @@ const GetStarted = () => (
       className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
     >
       <div className={`${styles.flexStart} flex-row`}>
-        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+        <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
           <span className="text-gradient">Get</span>
         </p>
         <img
           src={arrowUp}
-          alt="arrow"
+          alt="arrow-up"
           className="w-[23px] h-[23px] object-contain"
         />
       </div>
 
-      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+      <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
         <span className="text-gradient">Started</span>
       </p>
     </div>
